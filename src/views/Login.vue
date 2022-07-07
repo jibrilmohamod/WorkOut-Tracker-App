43f3fa4e9dcf64ad08a0454f3a45a2f9928819cb
Jibril Mohamod <jazimjibril@gmail.com>
2022-07-07 23:43:48 +0300

    initial commit

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+ <div>
+  <h1>login</h1>
+ </div>
+</template>
+
+<script>
+ export default {
+  name: "login",
+  setup() {
+   // Create data / vars
+
+   // Login function
+
+   return {}
+  },
+ }
+</script>
